de93ef13c72ff0b8a164b0663f6361ce090f8622
Howard <shoukeshook@gmail.com>
2023-02-01 12:04:38 +0100

    Adjusted delay

--- Labs/Lab1/files-lab1/timetemplate.asm
+++ Labs/Lab1/files-lab1/timetemplate.asm
@@ -84,14 +84,14 @@ hexasc:
   # supposedly one loop should take one ms
 onems:
 	addi $t0,$t0,1		# i++
-	blt $t0,$t1,onems	# checks i<t1, which in this case is 4
+	blt $t0,$a2,onems	# checks i<t1, which in this case is 4
 	nop			# once i = t1, we "fall thru" to the delay subroutine
 	
   # delay subroutine, loops as many times as a0 tells it. No outputs.
 delay:
 	subi $a0,$a0,1		# ms = ms - 1
 	li $t0, 0		# i = 0
-	li $t1, 4		# set the for loop constant
+	li $a2, 350		# set the for loop constant. 4 works on my 5800X, while 350 is needed on the M1 Max
 				# I assume this is the constant that needed to be adjusted to get this to equal 1000ms if a0 = 1000
 	bgtz $a0, onems		# the actual "while". It will jump to onems if ms/a0 is larger than 0
 	nop
