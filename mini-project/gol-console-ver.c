2834ba84a0b92ba3adb65e8637a4f8e296ae68a8
Howard <shoukeshook@gmail.com>
2023-02-15 14:23:18 +0100

    mini-proj: dymanic 2D array created

--- mini-project/gol-console-ver.c
+++ mini-project/gol-console-ver.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char** ppMatrix;
+
 int main(int argc, char *argv[]){
     // argc: Provice H and W for grid
-    printf("H: %d, W: %d", atoi(argv[1]), atoi(argv[2]));
+    int row = atoi(argv[1]);
+    int col = atoi(argv[2]);
+    printf("H: %d, W: %d\n", row, col);
 
-    char matrix[atoi(argv[1])][atoi(argv[2])];
-    char (*pMatrix)[atoi(argv[1])][atoi(argv[2])];;
-    pMatrix = &matrix;
+    ppMatrix = (char**)malloc(row * sizeof(char*));
+    for (int i = 0; i < row; i++) {
+        ppMatrix[i] = (char*)malloc(col * sizeof(char));
+    }
 
-    (*pMatrix)[1][2] =  "c";
+    for (int r = 0; r < row; r++) {
+        for (int c = 0; c < col; c++) ppMatrix[r][c] = 'o';
+    }
 
-    for (int i = 0; i < atoi(argv[1]); i++){
-        for (int j = 0; j < atoi(argv[2]); j++){
-            printf("%c", (*pMatrix)[i][j]);
-        }
+    ppMatrix[2][3] = '-';
+
+    for (int r = 0; r < row; r++) {
+        for (int c = 0; c < col; c++) printf("%2c", ppMatrix[r][c]);
         printf("\n");
     }
+
+    for (int r = 0; r < row; r++) {
+        free(ppMatrix[r]);
+    }
+    
+    free(ppMatrix);
     
 }
