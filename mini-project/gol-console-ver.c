f75e8ef3f2778aff7d63fec797decd6a53160514
Howard <shoukeshook@gmail.com>
2023-02-15 14:47:30 +0100

    mini-proj: added some helper functions for 2D array creation

--- mini-project/gol-console-ver.c
+++ mini-project/gol-console-ver.c
@@ -3,32 +3,50 @@
 
 char** ppMatrix;
 
+// free allocated memory
+void freeMatrix(int rows) {
+    for (int r = 0; r < rows; r++) {
+        free(ppMatrix[r]);
+    }
+    free(ppMatrix);
+}
+
+// Print the matrix. You could probably calculate the 
+void printMatrix(int rows, int cols) {
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) printf("%2c", ppMatrix[r][c]);
+        printf("\n");
+    }
+}
+
+// test function to see how accessing the 2D array works
+void editElement(int r, int c) {
+    ppMatrix[r-1][c-1] = 'o';
+}
+
+
 int main(int argc, char *argv[]){
     // argc: Provice H and W for grid
     int row = atoi(argv[1]);
     int col = atoi(argv[2]);
     printf("H: %d, W: %d\n", row, col);
 
+    // create the 2D array: a double pointer that points to an array of pointers
+    // that point to an array of chars. Cool stuff!
     ppMatrix = (char**)malloc(row * sizeof(char*));
     for (int i = 0; i < row; i++) {
         ppMatrix[i] = (char*)malloc(col * sizeof(char));
     }
 
+    // fill the 2D array with "dead cells". Just something so that it can be printed
     for (int r = 0; r < row; r++) {
-        for (int c = 0; c < col; c++) ppMatrix[r][c] = 'o';
+        for (int c = 0; c < col; c++) ppMatrix[r][c] = '-';
     }
 
-    ppMatrix[2][3] = '-';
+    editElement(2,3);
 
-    for (int r = 0; r < row; r++) {
-        for (int c = 0; c < col; c++) printf("%2c", ppMatrix[r][c]);
-        printf("\n");
-    }
+    printMatrix(row, col);
 
-    for (int r = 0; r < row; r++) {
-        free(ppMatrix[r]);
-    }
-    
-    free(ppMatrix);
+    freeMatrix(row);
     
 }
